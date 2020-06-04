ee00ef44ca14bff4856f2f6514c43ef2261fcb2a
Nathan Dorazil <nathand1915@gmail.com>
2020-06-04 23:52:56 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,6 +1,3 @@
 def oxford_comma(sentence)
-  fruit1 = ["kiwi"]
-  fruit2 = ["kiwi", "durian"]
-  fruit3 = ["kiwi", "durian", "starfruit"]
-  fruit5 = ["kiwi", "durian", "starfruit", "mangos", "dragon fruits", "lychees", "pomelos"]
+  
 end
